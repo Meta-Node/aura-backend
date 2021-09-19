1d7e5e8f5258406f5d84c9c0bdc1f58e6c0b3f17
Ashakibp <aaronshakib@gmail.com>
2021-09-19 16:09:23 -0400

    attempt to fix cors issue

--- app.js
+++ app.js
@@ -11,21 +11,11 @@ const {json} = require("express");
 var app = express();
 
 
-var allowedOrigins = ['localhost:3001',
-    'aura.brightid.org'];
-app.use(cors({
-    origin: function(origin, callback){
-        // allow requests with no origin
-        // (like mobile apps or curl requests)
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
+const corsConfig = {
+    credentials: true,
+    origin: true,
+};
+app.use(cors(corsConfig));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -33,7 +23,7 @@ app.use(cookieParser(process.env.SECRET));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", req.hostname);
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
