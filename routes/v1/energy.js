50278e2eb5517d27e40d95538776cad1c432c914
aaron shakibpanah <aaronshakib@gmail.com>
2022-03-30 17:01:12 +0100

    add energy

--- routes/v1/energy.js
+++ routes/v1/energy.js
@@ -1,7 +1,17 @@
 var express = require('express');
+const {getConnection} = require("../../src/controllers/connectionController");
 var router = express.Router();
 
 router.post('/', async function (req, res, next) {
+    let fromBrightId = req.body.fromBrightId;
+    let toBrightId = req.body.toBrightId;
+
+    let connection = (await getConnection(fromBrightId, toBrightId))[0];
+    if(connection === undefined) {
+        res.status(500).send("No connection between these two brightId");
+    }
+
+
 
 });
 
