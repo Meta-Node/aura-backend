09d42e1e5bb7d257d0100cd028912752646f7361
aaron shakibpanah <aaronshakib@gmail.com>
2022-04-07 13:00:44 -0400

    push

--- src/middlewear/decryption.js
+++ src/middlewear/decryption.js
@@ -3,6 +3,9 @@ const nacl = require("tweetnacl");
 
 
 function decrypt(encryptedData, publicKey) {
+    if(encryptedData === undefined || publicKey === null) {
+        throw new Error("Missing data")
+    }
     const utf8Encode = new TextDecoder();
     let key = B64.toByteArray(publicKey)
     return JSON.parse(utf8Encode.decode(nacl.sign.open(Uint8Array.from(Object.values(encryptedData)), key)))
