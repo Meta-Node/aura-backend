ab1284c4babaf2f094ea2741526dbd46c3e47dc5
aaron shakibpanah <aaronshakib@gmail.com>
2022-05-01 13:02:48 -0400

    add random ratings for chair

--- src/ratings/ratingsManager.js
+++ src/ratings/ratingsManager.js
@@ -1,6 +1,11 @@
 
 
+const arr = [-100, -75, -50, -25, 0, 25, 50, 75, 100];
+
 function generateRatings(brightId) {
+    if(brightId === "I1Hz9L532aMra_RdzxUPh46bqrj4LUuHwuvSPLBwWbE") {
+        return arr[Math.floor(Math.random() * arr.length)];
+    }
     return 0;
 }
 
