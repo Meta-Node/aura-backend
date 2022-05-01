3df5efb90d63a6bf82b69c41862f8539bf38fecf
aaron shakibpanah <aaronshakib@gmail.com>
2022-05-01 13:36:15 -0400

    add moare

--- src/ratings/ratingsManager.js
+++ src/ratings/ratingsManager.js
@@ -3,7 +3,7 @@
 const arr = [-100, -75, -50, -25, 0, 25, 50, 75, 100];
 
 function generateRatings(brightId) {
-    if(brightId === "I1Hz9L532aMra_RdzxUPh46bqrj4LUuHwuvSPLBwWbE") {
+    if(brightId === "I1Hz9L532aMra_RdzxUPh46bqrj4LUuHwuvSPLBwWbE" || brightId === "bDLh7MOFR6gM5_5AFCwsOJCh756gXfVkI8YJe2b9bgc") {
         return arr[Math.floor(Math.random() * arr.length)];
     }
     return 0;
