bbf96e4d86e8782cf77133add6ba677c28ffab12
Ali Parvizi <snparvizi75@gmail.com>
2022-08-08 02:54:29 -0400

    add updateAt field to ratings table

---
+++ migrations/1748090705381_add-updatedAt.js
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined
+
+exports.up = (pgm) => {
+  pgm.addColumns('ratings', {
+    updatedAt: { type: 'timestamp', notNull: false },
+  })
+  pgm.sql(
+    'UPDATE "ratings" as r1 SET "updatedAt"=(SELECT "createdAt" FROM "ratings" as r2 WHERE r1.id=r2.id)',
+  )
+  pgm.alterColumn('ratings', 'updatedAt', {
+    notNull: true,
+  })
+}
+
+exports.down = (pgm) => {
+  pgm.dropColumns('ratings', 'updatedAt')
+}
