b279c0843c981fb9a8e6dd443f205312d927a932
Ali Parvizi <snparvizi75@gmail.com>
2022-08-08 03:00:55 -0400

    add default for updatedAt

--- migrations/1748090705381_add-updatedAt.js
+++ migrations/1748090705381_add-updatedAt.js
@@ -10,6 +10,7 @@ exports.up = (pgm) => {
     'UPDATE "ratings" as r1 SET "updatedAt"=(SELECT "createdAt" FROM "ratings" as r2 WHERE r1.id=r2.id)',
   )
   pgm.alterColumn('ratings', 'updatedAt', {
+    default: pgm.func('current_timestamp'),
     notNull: true,
   })
 }
