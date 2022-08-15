0849f97407ec47a25e574adf1dcafc0a80547bfc
Ali Parvizi <snparvizi75@gmail.com>
2022-08-15 00:15:20 -0400

    check for invalid decoded data before passing it to json parser

--- src/middlewear/decryption.js
+++ src/middlewear/decryption.js
@@ -1,14 +1,18 @@
-const B64 = require("base64-js");
-const nacl = require("tweetnacl");
-
+const B64 = require('base64-js')
+const nacl = require('tweetnacl')
 
 function decrypt(encryptedData, publicKey) {
-    if(encryptedData === undefined || publicKey === null) {
-        throw new Error("Missing data")
-    }
-    const utf8Encode = new TextDecoder();
-    let key = B64.toByteArray(publicKey)
-    return JSON.parse(utf8Encode.decode(nacl.sign.open(Uint8Array.from(Object.values(encryptedData)), key)))
+  if (encryptedData === undefined || publicKey === null) {
+    throw new Error('Missing data')
+  }
+  const utf8Encode = new TextDecoder()
+  let key = B64.toByteArray(publicKey)
+  let payload = Uint8Array.from(Object.values(encryptedData))
+  let decoded = nacl.sign.open(payload, key)
+  if (decoded === undefined || decoded === null) {
+    throw new Error('Could not decode data')
+  }
+  return JSON.parse(utf8Encode.decode(decoded))
 }
 
-module.exports = {decrypt}
+module.exports = { decrypt }
